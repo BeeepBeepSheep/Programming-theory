a3d27d14e324af131a00e4b63f34bcf16c978c50
Jacob Lueke <jacoblueke@gmail.com>
2021-09-17 14:14:21 +0100

    no message

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace Program
             float myFloat = 420.69f;
             string myString = "a string of text";
             bool iKnowWhatImDoing = false;
+            int[] myArray = { 1, 2, 3, 4, };
 
             Console.WriteLine("my intiger is: " + myInt + ", my float is: " + myFloat + "my string example is: " + myString + ", my bool(i know what im doing)" + iKnowWhatImDoing);
             Console.ReadLine();
