8d6fea1842a26b243a61ddf9a46e52936613a54f
Jacob Lueke <jacoblueke@gmail.com>
2021-09-17 13:45:18 +0100

    no message

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 69;
+            float myFloat = 420.69f;
+            string myString = "a string of text";
+            bool iKnowWhatImDoing = false;
+
+            Console.WriteLine("my intiger is: " + myInt + ", my float is: " + myFloat + "my string example is: " + myString + ", my bool(i know what im doing)" + iKnowWhatImDoing);
+        }
+    }
+}
